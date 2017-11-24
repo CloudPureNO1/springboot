d0a624befbe083f876221c8d8a95c51dc8157e69
CloudPureNO1 <CloudPure@163.com>
2017-11-24 12:32:05 +0800

    sping boot 多模块集成jspp

--- web-jsp/src/main/java/com/cloudpure/springboot/web/JSPWebMvcConfig.java
+++ web-jsp/src/main/java/com/cloudpure/springboot/web/JSPWebMvcConfig.java
@@ -1,7 +1,19 @@
 package com.cloudpure.springboot.web;
 
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
 /**
  * Created by CloudPure on 2017/11/24.
+ * Sping Boot  多模块集成jsp 中，不仅要在application.properties 配置web路径，
+ * 还需要，继承WebMvcConfigurationAdapter的配置类
+ * 否则会找不到页面
  */
-public class JSPWebMvcConfig {
+public class JSPWebMvcConfig extends WebMvcConfigurerAdapter{
+
+    public InternalResourceViewResolver viewResolver(){
+        InternalResourceViewResolver viewResolver=new InternalResourceViewResolver();
+    }
+
+
 }
---
+++ web-jsp/src/main/java/com/cloudpure/springboot/web/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.cloudpure.springboot.web;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(WebJspApplication.class);
+	}
+
+}
